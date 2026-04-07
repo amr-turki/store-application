19fc85ba53390b9053195d2784f778e6f5d956fe
Amr Turki <amrturki79@gmail.com>
2026-04-07 08:09:53 +0200

    create store application

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const StoreApp());
+}
+
+class StoreApp extends StatelessWidget {
+  const StoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold());
+  }
+}
