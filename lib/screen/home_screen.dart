7296b3ef51b2597f7fa9d53f47442346fb62ed98
Amr Turki <amrturki79@gmail.com>
2026-04-09 16:13:18 +0200

    create product card

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -13,13 +13,78 @@ class HomeScreen extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {},
-            icon: Icon(CupertinoIcons.cart_badge_plus, color: Colors.black),
+            icon: Icon(
+              CupertinoIcons.cart_badge_plus,
+              color: Colors.black,
+              size: 24,
+            ),
           ),
         ],
         elevation: 0,
         backgroundColor: Colors.white,
         centerTitle: true,
-        title: Text('New Trend', style: TextStyle(color: Colors.black)),
+        title: Text(
+          'New Trend',
+          style: TextStyle(color: Colors.black, fontSize: 16),
+        ),
+      ),
+      body: Center(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 40,
+                    color: Colors.grey.withOpacity(0.4),
+                    spreadRadius: 0,
+                    offset: Offset(1, 1),
+                  ),
+                ],
+              ),
+              height: 150,
+              width: 220,
+              child: Card(
+                elevation: 10,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 16,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'HandBag Lv',
+                        style: TextStyle(color: Colors.black, fontSize: 16),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            r'$225',
+                            style: TextStyle(color: Colors.black, fontSize: 16),
+                          ),
+                          Icon(Icons.shopping_cart, color: Colors.black),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              right: 24,
+              top: -25,
+              child: Image.network(
+                'https://puzzlesegypt.com/cdn/shop/files/6B24B461-5E3B-4783-9614-22B932AFFAAE_555x.jpg?v=1775309939',
+                height: 100,
+              ), // Image.network
+            ), // Positioned
+          ],
+        ),
       ),
     );
   }
